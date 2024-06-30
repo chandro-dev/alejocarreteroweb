8cd782290bd239660522c596258c28486edbf21d
Luis alejandro Raptor <alejo_carretero@hotmail.com>
2024-06-30 07:40:44 -0500

    try fit vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/alejocarreteroweb/',
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
