786818690f47e27b6b98fd0e968396a7f5e71e8a
Luis alejandro Raptor <alejo_carretero@hotmail.com>
2024-06-30 07:57:20 -0500

    try fit vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import fotoPerfil from'./assets/image.png'
 function App() {
 
   return (
     <>
       <div>
-        <h1>Te amo mucho</h1>
+        <h1>El negro ama mucho a Lina</h1>
+        <img src={fotoPerfil}/>
+
       </div>
+
     </>
   )
 }
