827863f88f514e1081b726d5cfb529995a95ace7
alejoWork <alejo_carretero@hotmail.com>
2024-07-03 10:52:38 -0500

    add img for presentation

--- src/Presentation.jsx
+++ src/Presentation.jsx
@@ -1,17 +1,18 @@
 
-export default function Presentation(){
+import './Presentation.css'
+import foto from './assets/image.png'
+export default function Presentation() {
 
 
-    return(
-        <>
-        
-        <h2>Presentación</h2>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h1>
-       
-        </>
+    return (
+        <section id='sectionInformation'>
+
+            <h1>Soy Alejandro Carretero</h1>
+            <div id='containerInfo'>
+                <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h2>
+                <img className='imagen-enmarcada ' src={foto} alt="iamgen de perfil" />
+            </div>
+
+        </section>
     )
 }
