c97e0cddae84ddea862e3f502fc01956783eb95e
alejoWork <alejo_carretero@hotmail.com>
2024-07-03 16:10:22 -0500

    animate name

--- src/Presentation.jsx
+++ src/Presentation.jsx
@@ -7,7 +7,7 @@ export default function Presentation() {
     return (
         <section id='sectionInformation'>
 
-            <h1>Soy Alejandro Carretero</h1>
+            <h1 className='textoAnimado'>Soy Alejandro Carretero</h1>
             <div id='containerInfo'>
                 <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt laboriosam hic fugit delectus architecto odit neque accusamus nesciunt minus cumque nisi aperiam ducimus magni optio, exercitationem, voluptatum aliquid doloremque eius!</h2>
                 <img className='imagen-enmarcada ' src={foto} alt="iamgen de perfil" />
