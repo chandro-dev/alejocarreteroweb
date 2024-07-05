61f4e607326b48e358b6a15f037f15caf6ab140f
alejoWork <alejo_carretero@hotmail.com>
2024-07-05 08:37:12 -0500

    blur menu

--- src/HeaderSection.jsx
+++ src/HeaderSection.jsx
@@ -1,12 +1,12 @@
 import './HeaderSection.css'
 export default function HeaderSection() {
-    return (
-        <nav id="navHeader"> 
-                <ul>
-                <li>Presentación</li>
-                <li>Experiencia</li>
-                <li>Proyectos</li>
-                <li>Tecnologías</li>
-                </ul>
-        </nav>)
+        return (
+                <nav id="navHeader">
+                        <ul>
+                                <li><span>Presentación</span></li>
+                                <li><span>Experiencia</span></li>
+                                <li><span>Proyectos</span></li>
+                                <li><span>Tecnologías</span></li>
+                        </ul>
+                </nav>)
 }
