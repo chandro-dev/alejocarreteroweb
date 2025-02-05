95785c748f0cb02a84dff620297670df8045b322
Aejandro <alejocarreteroballesteros@gmail.com>
2025-02-04 22:56:13 -0500

    add light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Blog from "./blog/Blog";
 import Cv from "./cv/Cv";
 import Contact from "./contact/Contact";
 import './style.css'
+import { FaSun, FaMoon} from "react-icons/fa";
 
-function App() {  
+function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
   return (
+
+
+
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300">
+      {/* Botón de Dark Mode */}
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="absolute top-5 right-5 p-2 bg-gray-300 dark:bg-gray-700 rounded-full shadow-md transition-all"
+      >
+        {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+      </button>
+      {/* Rutas */}
       <Routes>
         <Route path="/alejocarreteroweb/" element={<Home />} />
         <Route path="/alejocarreteroweb/blog" element={<Blog />} />
